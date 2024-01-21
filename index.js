e165a4c2c21b55ab159133b2fd64ac2a2f85c94e
Pratham rathore <112610942+pratham90@users.noreply.github.com>
2024-01-21 18:17:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// let l1 = document.querySelector(".l1");
+// let l2 = document.querySelector(".l2");
+// let l3 = document.querySelector(".l3");
+// let l4 = document.querySelector(".l4");
+// let l5 = document.querySelector(".l5");
+// let l6 = document.querySelector(".l6");
+// let faq = document.querySelector(".faq");
+ 
+  
+// l1.addEventListener("click",function myFunction() {
+//     var element = document.getElementById("click1");
+//     element.classList.remove("a");
+//     faq.style.height="1024px";
+//   });
+// l2.addEventListener("click",function myFunction1() {
+//     var element = document.getElementById("click2");
+   
+//     element.classList.remove("b");
+//     element.classList.add("a");
+   
+//     faq.style.height="1024px";
+//   });
